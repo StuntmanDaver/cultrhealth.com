66737321fe0ea5fcf5d8266fa89eeebd7a95afd9
David K <ketchel.david@gmail.com>
2026-05-09 22:17:46 -0400

    Add first purchase discount config

---
+++ lib/config/first-purchase-discount.ts
@@ -0,0 +1,6 @@
+export const FIRST_PURCHASE_DISCOUNT_CODE = 'NEWCUSTOMER10'
+export const FIRST_PURCHASE_DISCOUNT_LABEL = 'New customer discount'
+export const FIRST_PURCHASE_DISCOUNT_PERCENT = 10
+
+export const FIRST_PURCHASE_DISCOUNT_NOTICE =
+  "You're getting 10% off your first purchase for being a new customer."
